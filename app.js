1d114e6a18eda79494e082eca33c2be8d97d7ba9
Hanzala-Ahmed <hanzala.ahmed181@gmail.com>
2023-06-18 23:05:21 +0500

    fix: url

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require("path");
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://ecommerce-han.vercel.app"],
     credentials: true,
   })
 );
